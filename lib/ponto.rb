ff5daac6d07e8e56055c817c73a47384c9aefcb2
Júlio Campos <julio.campos@bioritmo.com.br>
2020-10-17 12:58:56 -0300

    First release

--- lib/ponto.rb
+++ lib/ponto.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sqlite3'
+
+DB = SQLite3::Database.new 'appointments.db'
+
+DB.execute <<-SQL
+  create table if not exists daily (
+    type varchar(10),
+    time varchar(30)
+  );
+SQL
 
 CHOICES = {
   'entrada' => 'Entrada',
   'saida' => 'Saída',
-  'almoco' => 'Almoço'
+  'almoco' => 'Saída para Almoço',
+  'almoco2' => 'Retorno do Almoço'
 }.freeze
 
 get '/' do
@@ -15,5 +26,15 @@ end
 post '/ponto' do
   @title = 'Ponto lançado'
   @type = params['type']
+  @time = DateTime.now.to_s
+
+  DB.execute 'INSERT into daily values (?, ?)', @type, @time
+
   erb :ponto
+end
+
+get '/results' do
+  @appointments = DB.execute 'select * from daily'
+  @title = 'Fechamento do Dia'
+  erb :results
 end
